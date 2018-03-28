1237cb06ec656032bbbd78b3ef504fc8e94c3d71
bona <baishi.bona@gmail.com>
2018-03-28 12:25:47 -0400

    Update inf points with intensity -1

--- src/scan_to_pcl.cpp
+++ src/scan_to_pcl.cpp
@@ -28,8 +28,29 @@ laser_geometry::LaserProjection projector;
 
 void hokuyo_callbacks(const sensor_msgs::LaserScan::ConstPtr& scan_in)
 {
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud2(new pcl::PointCloud<pcl::PointXYZI>());
+    pcl::PointXYZI fake_hit;
     sensor_msgs::PointCloud2 cloud;
     projector.projectLaser(*scan_in, cloud);
+
+    // convert message to pcl2
+    pcl::fromROSMsg(cloud, cloud2);
+
+    // Add inf points
+    for(int i=0;i<scan_in->ranges.size();i++) {
+        if(isnan(scan_in->ranges[i])) {
+            double r = scan_in->ranges[i];
+            double theta = scan_in->angle_min + double(i) * scan_in->angle_increment;
+            fake_hit.x() = r * cos(theta);
+            fake_hit.y() = r * sin(theta);
+            fake_hit.z() = 0;
+            fake_hit.intensity() = -1.0;
+        }
+        cloud2->push_back(fake_hit);
+    }
+
+    // convert pcl2 back to message
+    pcl::toROSMsg(*cloud2, cloud);
 
     // Publish the new point cloud.
     cloud.header.frame_id = "/laser";
